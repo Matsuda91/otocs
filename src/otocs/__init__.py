ad2d4f7a60a9d8757b86017b845948a84ec4d56f
RyoMatsuda <matsuda.klab@gmail.com>
2026-01-25 22:25:55 +0900

    fix: reorganize imports and update __all__ in __init__.py

--- src/otocs/__init__.py
+++ src/otocs/__init__.py
@@ -1,3 +1,19 @@
-from .circuit import circuit
-from .model import model
+from .circuit import (
+    QuantumStateManager,
+    SweepEchoKResult,
+    otoc_circuit,
+    sweep_echo_k,
+)
+from .model import Observable
 from .fujii_arxiv import truncated_propagator
+from .style import apply_template
+
+
+__all__ = [
+    "SweepEchoKResult",
+    "sweep_echo_k",
+    "Observable",
+    "truncated_propagator",
+]
+
+apply_template("default")
