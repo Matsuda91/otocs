cc1783b5b30c843c752481cf7065e45695149167
RyoMatsuda <matsuda.klab@gmail.com>
2026-01-26 11:26:21 +0900

    refactor: update __all__ to include compute_fig1_theta_histograms and compute_fig2_moments, remove unused imports in __init__.py

--- src/otocs/__init__.py
+++ src/otocs/__init__.py
@@ -1,19 +1,19 @@
 from .circuit import (
-    QuantumStateManager,
-    SweepEchoKResult,
-    otoc_circuit,
     sweep_echo_k,
 )
 from .model import Observable
-from .fujii_arxiv import truncated_propagator
+from .fujii_arxiv import (
+    compute_fig1_theta_histograms,
+    compute_fig2_moments,
+)
 from .style import apply_template
 
 
 __all__ = [
-    "SweepEchoKResult",
     "sweep_echo_k",
     "Observable",
-    "truncated_propagator",
+    "compute_fig1_theta_histograms",
+    "compute_fig2_moments",
 ]
 
 apply_template("default")
